212e7ae2ec58168ee6eafb9b2ec5135bd3a7c4f9
Jaroslaw Lewandowski <jotel@jotel.net>
2013-09-03 23:44:03 +0100

    some changes

---
+++ Test2/MyView.h
@@ -0,0 +1,13 @@
+//
+//  MyView.h
+//  Test2
+//
+//  Created by Jaroslaw Lewandowski on 03/09/2013.
+//  Copyright (c) 2013 Jotel Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyView : UIView
+
+@end
